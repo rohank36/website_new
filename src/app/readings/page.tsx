7f2e3ecda7721ac758fe11af7deee560b22502b2
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2024-07-21 16:57:15 -0400

    more

--- src/app/readings/page.tsx
+++ src/app/readings/page.tsx
@@ -1,5 +1,15 @@
 'use client'
+import Navbar from "../Navbar";
 
 export default function Readings() {
-  return (<h1>Readings</h1>);
+  return (
+    <div className="bg-black min-h-screen">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center text-white text-sm min-h-screen pb-8">
+        <div className="flex flex-col items-start text-left max-w-xl space-y-4">
+          <p>Readings</p>
+        </div>
+      </div>
+    </div>
+  );
 }
